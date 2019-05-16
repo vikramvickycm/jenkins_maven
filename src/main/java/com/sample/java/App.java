79734dd6f122a4408cae87d429562ac294831602
vikramcm <vikramcmhr@gmail.com>
2019-05-16 16:58:58 +0530

    project created on may 16th

---
+++ src/main/java/com/sample/java/App.java
@@ -0,0 +1,15 @@
+package com.sample.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "TTD");
+        System.out.println( "TTD");
+        System.out.println( "Hello world");
+    }
+}
